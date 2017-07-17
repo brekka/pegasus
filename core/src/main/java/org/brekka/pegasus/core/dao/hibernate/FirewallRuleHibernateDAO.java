dc77bedce3f5faed18de70b85c1a36d9fa48cf2c
Andrew Taylor <andrew@brekka.org>
2017-07-17 13:50:25 +0000

    Need to use '>>=' operator to enable matching single IPs

--- core/src/main/java/org/brekka/pegasus/core/dao/hibernate/FirewallRuleHibernateDAO.java
+++ core/src/main/java/org/brekka/pegasus/core/dao/hibernate/FirewallRuleHibernateDAO.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.brekka.pegasus.core.dao.hibernate;
 
@@ -18,43 +18,33 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class FirewallRuleHibernateDAO extends AbstractPegasusHibernateDAO<FirewallRule> implements FirewallRuleDAO {
 
-    /* (non-Javadoc)
-     * @see org.brekka.commons.persistence.dao.impl.AbstractIdentifiableEntityHibernateDAO#type()
-     */
     @Override
     protected Class<FirewallRule> type() {
         return FirewallRule.class;
     }
-    
-    /* (non-Javadoc)
-     * @see org.brekka.pegasus.core.dao.FirewallDAO#isAccessAllowed(java.lang.String, org.brekka.pegasus.core.model.Firewall)
-     */
-    @SuppressWarnings({ "unchecked", "cast" })
+
+    @SuppressWarnings({ "unchecked" })
     @Override
-    public List<FirewallRule> findApplicableRules(Firewall firewall, String ipAddress) {
+    public List<FirewallRule> findApplicableRules(final Firewall firewall, final String ipAddress) {
         SQLQuery query = getCurrentSession().createSQLQuery(
                 "SELECT fr.* " +
                 "  FROM \"Pegasus\".\"FirewallRule\" fr" +
                 "  JOIN \"Pegasus\".\"Network\" ipn" +
                 "    ON  fr.\"NetworkGroupID\" = ipn.\"NetworkGroupID\"" +
-                " WHERE ipn.\"Block\" >> cast(:ip as cidr)" +
+                " WHERE ipn.\"Block\" >>= cast(:ip as cidr)" +
                 "   AND fr.\"FirewallID\" = cast(:firewall as uuid)" +
                 " ORDER BY fr.\"Priority\" ASC");
         query.setParameter("ip", ipAddress);
         query.setString("firewall", firewall.getId().toString());
         query.addEntity(FirewallRule.class);
-        return (List<FirewallRule>) query.list();
+        return query.list();
     }
-    
-    /* (non-Javadoc)
-     * @see org.brekka.pegasus.core.dao.FirewallRuleDAO#retrieveForFirewall(org.brekka.pegasus.core.model.Firewall)
-     */
+
     @SuppressWarnings("unchecked")
     @Override
-    public List<FirewallRule> retrieveForFirewall(Firewall firewall) {
+    public List<FirewallRule> retrieveForFirewall(final Firewall firewall) {
         return getCurrentSession().createCriteria(FirewallRule.class)
                 .add(Restrictions.eq("firewall", firewall))
                 .list();
     }
-
 }
